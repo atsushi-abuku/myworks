d31edfe25a94f5f303aba31b8c019f7ddc228a9f
atsushi-abuku <atsushijump623@gmail.com>
2023-03-07 15:24:29 +0900

    [Add] index

---
+++ Character.cpp
@@ -0,0 +1,18 @@
+﻿#include "stdafx.h"
+#include "Character.h"
+
+Character::Character(float x,float y,int id) {
+	this->id = id;
+	this->x = x;
+	this->y = y;
+}
+
+Character* Character::increase() {
+	Character *chara = new Character(this->x, this->y, this->id);
+	return chara;
+}
+
+void Character::move() {
+	this->x += Random(-2.0f, 2.0f);
+	this->y += Random(-2.0f, 2.0f);
+}
---
+++ Character.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+class Character
+{
+	public:
+		Character(float x, float y,int id);
+		Character* increase();
+		void move();
+		float x;
+		float y;
+	protected:
+		int id;
+		
+
+};
+
